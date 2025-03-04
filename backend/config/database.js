431e95e36ea1ffdc5431866e2427200f572ce792
Nicolaspereztaf <mariaangelpereztafur@gmail.com>
2025-03-04 08:18:40 -0500

    segunda actualizacion

---
+++ backend/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+const URI = `mongodb+srv://${process.env.USERBD}:${process.env.PASSBD}@adso2903013.jnezx.mongodb.net/${process.env.BD}`
+
+mongoose.connect(URI)
+
+module.exports = mongoose;
